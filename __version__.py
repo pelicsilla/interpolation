ab5ffac54c5f839cb4f5eb25407a543f2975619f
Csilla Peline <peline.csilla@gmail.com>
2024-12-01 14:11:44 +0100

    Add CHANGELOG.md, requirements.txt, __version__.py, .gitignore

--- __version__.py
+++ __version__.py
@@ -4,8 +4,8 @@ __version__ = "0.0.1"
 description = ("Input temperature (csv) data is interpolated and visualized "
                "using MetPy, cartopy, matplotlib, numpy and pandas packages, and saved to png.")
 python = "3.12"
-install_requires = "MetPy, cartopy, matplotlib, numpy, pandas"
+install_requires = "requirements.txt"
 features ={
-    "MATH-1": ["Implement temperature interpolation and mapping"],
-    "MATH-2": ["Implement add metpy logo on the map"]
+    "INTPOL-1": ["Implement temperature interpolation and mapping"],
+    "INTPOL-2": ["Implement add metpy logo on the map"]
     }
